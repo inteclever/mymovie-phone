91bbd022e54fdd51474157b3172ab36abb2c6e94
na-losonogov <nlosonogov@sviaz-bank.ru>
2018-11-08 15:21:37 +0700

    auth page edits

--- www/js/script.js
+++ www/js/script.js
@@ -1,4 +1,11 @@
 ﻿
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home }
+  ]
+})
+
 Vue.component('menu-items',{
 	props:['name'],
 	template:'<span>{{name}}</span>'
@@ -40,7 +47,6 @@ var app = new Vue({
 	},
 	methods:{
 		animation: function(){
-			console.log('chaah');
 			var count = this.messages.length;
 			this.messages.push({text:'Vanclav '+parseInt(count)+1, id:parseInt(count)+1});
 		},
@@ -48,10 +54,8 @@ var app = new Vue({
 			var oldMessages = this.messages;  
 			var part = this.messages.slice(1,5);
 			this.messages = part;
-			console.log(oldMessages);
 			for(item in oldMessages) this.messages.push({id:oldMessages[item].id, text:oldMessages[item].text});		
 		}
-		//openMenu: function(){this.menu_enable = (this.menu_enable)?false:true; console.log(this.menu_enable);}
 	},
 	
 	computed: {
@@ -59,25 +63,38 @@ var app = new Vue({
     mounted() {
     }
 })
-var app = new Vue({
+var app2 = new Vue({
 	el: '.authContainer',
 	data: {
 		valid: true,
+		password: null, 
 		passwordRules: [
 			v => !!v || 'Обязательное поле',
 			v => (v && v.length >= 5) || 'Пароль должен быть более 5 символов'
+		],
+		repassword: null,
+		repasswordRules: [
+			v => !!v || 'Обязательное поле',
+			v => app2.password == app2.repassword || 'Должно совпадать с полем "Пароль"'
 		],
 		email: '',
 		emailRules: [
 			v => !!v || 'Обязательное поле',
 			v => /.+@.+/.test(v) || 'Пожалуйста, введите email'
-		]
+		],
+		checkbox: false,
+		retryPasswordFlag: false, 
+		formLabel: "Авторизируйтесь",
+		buttonSubmit: "Регистрация", 
+		submitUrl: "registration",
+		comparePasswordFlag: false
 	},
 	methods:{
 		submit () {
-			if (this.$refs.form.validate()) {
+			if (this.$refs.form.validate() && comparePasswordFlag) {
+				console.log(`Email: ${this.email} passwrod ${this.password} this: ${this}`);
 				// Native form submission is not yet supported
-					axios.post('/api/submit', {
+					axios.post('/https://quicknote.bget.ru/user/'+this.submitUrl, {
 					email: this.email,
 					password: this.password
 				})
@@ -85,6 +102,17 @@ var app = new Vue({
 		},
 		clear () {
 			this.$refs.form.reset()
+		},
+		registration (){
+			this.buttonSubmit = (this.retryPasswordFlag) ? "Регистрация" : "Авторизация";
+			this.formLabel = (this.retryPasswordFlag) ? "Авторизируйтесь" : "Зарегистрируйтесь";
+			this.submitUrl = (this.retryPasswordFlag) ? "authorization" : "registration";
+			this.retryPasswordFlag = !this.retryPasswordFlag;
+		},
+		comparePassword(){
+			console.log(`Email: ${this.password} passwrod ${this.repassword} `);
+			comparePasswordFlag = this.password == this.repassword;
+			console.log(comparePasswordFlag);
 		}
 	},
 	
