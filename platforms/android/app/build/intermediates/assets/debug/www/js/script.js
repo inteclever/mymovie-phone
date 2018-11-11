42abd5d2709678f9118a205d7e80385c0d7ac7ab
inteclever <inteclever@gmail.com>
2018-11-11 15:55:41 +0700

    chang auth proc

--- platforms/android/app/build/intermediates/assets/debug/www/js/script.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/script.js
@@ -1,4 +1,5 @@
 ﻿
+
 Vue.component('menu-items',{
 	props:['name'],
 	template:'<span>{{name}}</span>'
@@ -40,7 +41,6 @@ var app = new Vue({
 	},
 	methods:{
 		animation: function(){
-			console.log('chaah');
 			var count = this.messages.length;
 			this.messages.push({text:'Vanclav '+parseInt(count)+1, id:parseInt(count)+1});
 		},
@@ -48,10 +48,76 @@ var app = new Vue({
 			var oldMessages = this.messages;  
 			var part = this.messages.slice(1,5);
 			this.messages = part;
-			console.log(oldMessages);
 			for(item in oldMessages) this.messages.push({id:oldMessages[item].id, text:oldMessages[item].text});		
 		}
-		//openMenu: function(){this.menu_enable = (this.menu_enable)?false:true; console.log(this.menu_enable);}
+	},
+	
+	computed: {
+    },
+    mounted() {
+    }
+})
+var app2 = new Vue({
+	el: '.authContainer',
+	data: {
+		valid: true,
+		password: null, 
+		passwordRules: [
+			v => !!v || 'Обязательное поле',
+			v => (v && v.length >= 5) || 'Пароль должен быть более 5 символов'
+		],
+		repassword: null,
+		repasswordRules: [
+			v => !!v || 'Обязательное поле',
+			v => app2.password == app2.repassword || 'Должно совпадать с полем "Пароль"'
+		],
+		email: '',
+		emailRules: [
+			v => !!v || 'Обязательное поле',
+			v => /.+@.+/.test(v) || 'Пожалуйста, введите email'
+		],
+		checkbox: false,
+		retryPasswordFlag: false, 
+		formLabel: "Авторизируйтесь",
+		buttonSubmit: "Регистрация", 
+		submitUrl: "auth",
+		request: ""
+	},
+	methods:{		
+		submit () {
+			if (this.$refs.form.validate()) {
+				console.log(`Email: ${this.email} passwrod ${this.password} action: ${this.submitUrl}`);
+				// Native form submission is not yet supported
+					axios.get('http://quicknote.bget.ru/', {
+						params:{
+							email: this.email,
+							password: this.password,
+							action:this.submitUrl
+						}
+				}).then(response => {
+					console.log(response);
+					let request = response.data;
+					this.request = response.data.info;
+					if(request.status) sqLiteAddUser(request);
+				}).catch(error => {					
+					console.log(error);
+					this.request = response.data.info;
+				});
+			}
+		},
+		clear () {
+			this.$refs.form.reset()
+		},
+		registration (){
+			this.buttonSubmit = (this.retryPasswordFlag) ? "Регистрация" : "Авторизация";
+			this.formLabel = (this.retryPasswordFlag) ? "Авторизируйтесь" : "Зарегистрируйтесь";
+			this.submitUrl = (this.retryPasswordFlag) ? "auth" : "addUser";
+			this.retryPasswordFlag = !this.retryPasswordFlag;
+			this.request = "";
+		},
+		sqLiteAddUser(data){
+			
+		}
 	},
 	
 	computed: {
---
+++ platforms/android/app/build/intermediates/assets/debug/www/templates/addMovie.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="itroot">
+	
+</div>
+</template>
