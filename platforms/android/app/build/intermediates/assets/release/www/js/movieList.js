1d1f271d167ce30d8ff6907104be2331c84b2419
inteclever <inteclever@gmail.com>
2018-11-13 21:27:59 +0700

    platform edits

---
+++ platforms/android/app/build/intermediates/assets/release/www/js/movieList.js
@@ -0,0 +1,183 @@
+﻿
+
+Vue.component('menu-items',{
+	props:['name'],
+	template:'<span>{{name}}</span>'
+});
+Vue.component('types-list',{
+	props:['type'],
+	template:'<option >{{type}}</option>'
+});
+Vue.component('block-page',{
+  template: './templates/addMovie.vue'
+})
+
+var app = new Vue({
+	el: '.movieContainer',
+	data: {
+		db: window.openDatabase("Database", "1.0", "mymoviedb", 200000),
+		userData: {},
+		menuEnable: null, // close or open main menu
+		menu:[
+			{name:'Добавить фльм', id:1, icon:'add_circle_outline', href:'addMovie.html'},
+			{name:'Мои фильмы', id:2, icon:'ondemand_video', href:'movieList.html'},
+			{name:'Мои сериалы', id:3, icon:'personal_video', href:'movieList.html'}
+		],
+		menuContext:[
+			{name:'Выйти', id:1},
+			{name:'Закрыть', id:2}
+		],
+		/* container for film list of user*/
+		films:[],
+		searchedFilms:[],
+		selectedSearchFilm: null,
+		menuItem_class:'menu-item',
+		htmlRequest: null,
+		showCards: true,
+		enableSearchMovieButton: false,
+		searchMovieButtonName: "Найти фильм",
+		filmsLoadingButton: "Подождите, загружаю схожие фильмы...",
+		searchMessage: "Список найденых фильмов",
+		description: "",
+		seenStyle: null
+	},
+	methods:{
+		/* Click right up context menu. Events. */
+		clickContextMenu(id){
+			if(id == 1){
+				/* IF USER CLICK EXIT ON CONTEXT MENU */
+				this.db.transaction(function(tx){
+					tx.executeSql('DROP TABLE IF EXISTS users');
+					window.location.replace("index.html");	
+				});			
+			}else if(id == 2){
+				
+			}
+		},
+		/* DELETE MOVIE FROM DATA BASE */
+		deleteFromList(id, counter){
+			console.log(`id film: ${id} id user ${this.userData.id} counter: ${counter}`);
+			axios.get('http://quicknote.bget.ru/', {
+				params:{
+					id_film: id,
+					id_user: this.userData.id,
+					action:'deleteFilm'
+				}
+			}).then(response => {
+				
+				let request = response.data;
+				if(request.status) this.films.splice(counter, 1);
+				
+			}).catch(error => {					
+				console.log(error);
+			});
+		},
+		/* GET USER DATA FROM SQLITE */
+		checkLogin(){
+			try{				
+				this.db.transaction(function(tx){	
+					tx.executeSql('SELECT * FROM users', [], 
+					function(tx, result){
+						app.userData = result.rows[0];
+						app.getListFilm();
+					}, null);	
+				}, this.errorCB, null)	
+			}catch(ex){ console.log('AUTH ERROR: '+ex);	}			
+		},
+		/* GET LIST OF FILMS */
+		getListFilm(){
+			axios.get('http://quicknote.bget.ru/', {
+				params:{
+					id_user: this.userData.id,
+					action:'getListFilm'
+				}
+			}).then(response => {
+				let request = response.data;
+				app.films = request.list;
+				this.htmlRequest = response.data.info;
+			}).catch(error => {					
+				console.log(error);
+			});
+		},
+		searchFilm(name, id_film){
+			this.searchedFilms = [];
+			this.searchMessage = null;
+			this.showCards = !this.showCards;
+			axios.get('http://quicknote.bget.ru/', {
+				params:{
+					key: name,
+					action:'getFilm'
+				}
+			}).then(response => {
+				this.searchedFilms = response.data;
+				this.searchMessage = (this.searchedFilms.length == 0) ? "К соажлению, поиск не дал результатов. Попробуйте указать более точное название фильма или добавить к названию год выпуска" : this.searchMessage;
+				this.filmsLoadingButton = "Назад";
+				this.selectedSearchFilm = id_film;
+			}).catch(error => {					
+				console.log(error);
+			});
+		},
+		bindMovie(film, idFilm){
+			axios.get('http://quicknote.bget.ru/', {
+				params:{
+					id_user: this.userData.id,
+					id_film: this.selectedSearchFilm,
+					description: film.description,
+					kp_id: film.id,
+					img: film.imgSrc,
+					name: film.name,
+					rating: film.rating,
+					url: film.url,
+					date: film.date,
+					action:'bindMovie'
+				}
+			}).then(response => {
+				let request = response.data;
+				app.films = request.list;
+				this.htmlRequest = response.data.info;
+				document.getElementById('textfilm-'+idFilm).innerHTML = response.data.info;
+			}).catch(error => {					
+				console.log(error);
+			});
+		},
+		getDescriptionFilm(url, idFilm){
+			document.getElementById('describe-'+idFilm).innerHTML = "Загружаю...";
+			axios.get('http://quicknote.bget.ru/', {
+				params:{
+					url: url,
+					action:'getDescription'
+				}
+			}).then(response => {
+				//film_description = response.data.description;
+				//document.getElementById(idFilm).innerHTML = response.data.description;
+				this.searchedFilms[idFilm].description = response.data.description;
+				document.getElementById('describe-'+idFilm).style.display="none";
+			}).catch(error => {					
+				console.log(error);
+				document.getElementById('describe-'+idFilm).innerHTML = "Ошибка, попробуйте позже";
+			});			
+		},
+		seenMovie(id_film, counter){
+			console.log(`id film: ${id_film} id user ${this.userData.id} counter: ${counter}`);
+			axios.get('http://quicknote.bget.ru/', {
+				params:{
+					id_film: id_film,
+					id_user: this.userData.id,
+					action:'seenFilm'
+				}
+			}).then(response => {				
+				let request = response.data;
+				if(request.status) this.films[counter].seen = 1;
+				
+			}).catch(error => {					
+				console.log(error);
+			});
+		}
+	},
+	
+	computed: {
+    },
+    mounted() {
+		this.checkLogin()
+    }
+})
---
+++ platforms/android/app/build/intermediates/assets/release/www/js/script.js
@@ -0,0 +1,117 @@
+﻿
+
+Vue.component('menu-items',{
+	props:['name'],
+	template:'<span>{{name}}</span>'
+});
+Vue.component('types-list',{
+	props:['type'],
+	template:'<option >{{type}}</option>'
+});
+Vue.component('block-page',{
+  template: './templates/addMovie.vue'
+})
+
+var app = new Vue({
+	el: '.movieContainer',
+	data: {
+		db: window.openDatabase("Database", "1.0", "mymoviedb", 200000),
+		userData: {},
+		movieNameRule: [v => (v && v.length > 0) || 'Введите название фильма'],
+		typeMovieSelected:1,
+		typeMovie:[
+			{name:'Фильм', id:1},
+			{name:'Сериал', id:2}
+		],
+		menu:[
+			{name:'Добавить фльм', id:1, icon:'add_circle_outline', href:'addMovie.html'},
+			{name:'Мои фильмы', id:2, icon:'ondemand_video', href:'movieList.html'},
+			{name:'Мои сериалы', id:3, icon:'personal_video', href:'movieList.html'}
+		],
+		menuContext:[
+			{name:'Выйти', id:1},
+			{name:'Закрыть', id:2}
+		],
+		still: 'It still text',
+		vunch_css:'block',
+		menu_class:'menu',
+		menuItem_class:'menu-item',
+		movieName: null,
+		menuEnable: null, // close or open main menu
+		compiled: null, 
+		htmlRequest: null,
+		enableAddMovieButton: false,
+		addMovieButtonName: "Добавить"
+		
+	},
+	methods:{
+		animation: function(){
+			var count = this.messages.length;
+			this.messages.push({text:'Vanclav '+parseInt(count)+1, id:parseInt(count)+1});
+		},
+		reverse: function(){
+			var oldMessages = this.messages;  
+			var part = this.messages.slice(1,5);
+			this.messages = part;
+			for(item in oldMessages) this.messages.push({id:oldMessages[item].id, text:oldMessages[item].text});		
+		},
+		/* Click right up context menu. Events. */
+		clickContextMenu(id){
+			if(id == 1){
+				/* IF USER CLICK EXIT ON CONTEXT MENU */
+				this.db.transaction(function(tx){
+					tx.executeSql('DROP TABLE IF EXISTS users');
+					window.location.replace("index.html");	
+				});			
+			}else if(id == 2){
+				
+			}
+		},
+		/* Add new film or serial to data base */
+		saveMovie(){
+			app.enableAddMovieButton = true;
+			app.addMovieButtonName = "Ожидайте..";
+			if (this.typeMovieSelected && this.movieName) {
+				// Native form submission is not yet supported
+				axios.get('http://quicknote.bget.ru/', {
+					params:{
+						id_user: this.userData.id,
+						id_type: this.typeMovieSelected,
+						movie_name: this.movieName,
+						action:'addMovie'
+					}
+				}).then(response => {
+					console.log(response);
+					let request = response.data;
+					this.htmlRequest = response.data.info;
+					app.enableAddMovieButton = false;
+					app.addMovieButtonName = "Добавить";
+				}).catch(error => {					
+					console.log(error);
+					app.enableAddMovieButton = false;
+					app.addMovieButtonName = "Добавить";
+				});
+			}else{
+				console.log(this.$refs);
+				app.enableAddMovieButton = false;
+			}
+		},
+		/* GET USER DATA FROM SQLITE */
+		checkLogin(){
+			try{				
+				this.db.transaction(function(tx){	
+					tx.executeSql('SELECT * FROM users', [], 
+					function(tx, result){
+						app.userData = result.rows[0];
+					}, null);	
+				}, this.errorCB, null)	
+			}catch(ex){ console.log('AUTH ERROR: '+ex);	}			
+		}
+	},
+	
+	computed: {
+    },
+    mounted() {
+		this.checkLogin()
+    }
+})
